6e45a2201fd6629bea6fcd5b9a0c5ebfeb3e3562
TOcta <toaderoctavian@yahoo.com>
2025-08-14 16:32:31 +0300

    aleluia

---
+++ Lesson2.py
@@ -0,0 +1,27 @@
+print("Hello world!")
+#de afisat numele un hobby animalul preferat orasul preferat si culoarea preferata#
+# print("Nume: \nToader Octavian")
+# print ("Hobby: \t mixat muzica")
+# print("Animalul preferat: Pisica")
+# print ("Orasul preferat: Roma")
+# print ("Culoarea preferata: Albastru")
+
+# \n = new line se foloseste pentru a pune pe alta linie ce este dupa sintaxa respectiva, de notat ca e vorba de backslash
+# \t = distantare cu tab
+
+#exercitiul 1 print poezie cu 4 linii in terminal
+
+print ("asta e casa \ncasa mea \ncasa frumoasa \ncasa mea")
+
+#TODO highlight pt ce ai de facut
+
+# CTRL+/ = Comment pentru tot ce selectezi
+
+#Tipuri de date 
+#string - insiruire de caractere
+Octa
+#int - integer nr intreg (nu contine zecimale)
+7
+#float - nr cu zecimale
+7,2333
+#bool = boolean= adevarat/fals
