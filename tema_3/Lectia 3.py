131404edf69646732173ab0782be551796f84f07
TOcta <toaderoctavian@yahoo.com>
2025-08-14 17:47:08 +0300

    tema  cu modificari

--- tema_3/Lectia 3.py
+++ tema_3/Lectia 3.py
@@ -18,19 +18,22 @@
     
 
 
-usercor=str("Octavian")
-passwordcor=str("1234")
+usercor = "Octavian"
+passwordcor = "1234"
 
-user= str(input("Va rog sa introduceti numele de utilizator si parola\nUser: "))
-password=str(input("Password:") )
+user = input("Va rog sa introduceti numele de utilizator si parola\nUser: ")
+password = input("Password: ")
 
 # chiar daca scriu userul gresit imi ia mai intai in functie de cum este scris codul, trebuie avut grija la cum sunt pozitionate conditiile ptc este posibil
 # sa se anuleze singura pana ajunge la conditia finala
 if user != usercor and password !=passwordcor:
     print("Acces respins")
-elif password != passwordcor:
-    print("Password incorect")
-elif usercor != user:
-    print("User incorect")   
+elif usercor != user or password !=passwordcor:
+    print("ser/Password incorect")   
 elif user ==usercor and password ==passwordcor:
     print("Acces permis")
+
+# if usercor != user:
+#     print("User incorect")   
+# if password != passwordcor:
+#     print("Password incorect")
